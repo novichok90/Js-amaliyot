4c9cb2b9b0bfd1dfb273a9a7e2bf4da988d29c1f
Kamron <odilovkamron14@gmail.com>
2026-04-09 21:19:19 +0500

    JS amaliyot

---
+++ app.js
@@ -0,0 +1,126 @@
+
+let a = 56
+let b = 39
+let c = (a+b)
+console.log(c)
+
+let a1 = 100
+let b1 = 9
+let c1 = (a1-b1)
+console.log(c1)
+
+let q = 54
+let w = 45
+let e = (q*w)
+console.log(e)
+
+let r = 54
+let t = 9
+let y = (r/t)
+console.log(y)
+
+let u = 45
+let i = 2
+let o = (u**i)
+console.log(o)
+
+let p = 20
+let a2 = 30
+let c2 = 2
+let s = (p+a2)*2
+console.log(s)
+
+let d = 50
+let f = 10
+let g = 3
+let h = d-f*g
+console.log(h)
+
+let j = 100
+let k = 50
+let l = 5
+let z = (j/k)+l
+console.log(z)
+
+let x = 34
+let v = 3
+let b2 = 45
+let n = (x**v)*b2
+console.log(n)
+
+let m = 8
+let u1 = 2
+let t1 = 5
+let c3 = (m/u1)**t1
+console.log(c3)
+
+let son1 = 10
+let son2 = 20
+let kichik = son1 < son2
+console.log(kichik)
+
+let number = 30
+let number1 = 40
+let teng = 30 >= 40
+console.log(teng)
+
+let son3 = 50
+let son4 = 40
+let katta = 50>40
+console.log(katta)
+
+let x1 = 5
+let x2 = 3
+let kopaytma = x1 * x2
+let d1 = 20
+let taqqos = kopaytma <= d1
+console.log(taqqos)
+
+let x3 = 18
+let x4 = 3
+let kopaytma1 = x3 / x4
+let d2 = 5
+let taqqos1 = kopaytma >= d2
+console.log(taqqos1)
+
+let x5 = 20
+let x6 = 20
+let kopaytma2 = x5 + x6
+let d3 = 10
+let taqqos2 = kopaytma2 < d3
+console.log(taqqos2)
+
+let x7 = 10
+let x8 = 5
+let kopaytma3 = x7 - x8
+let d4 = 3
+let taqqos3 = kopaytma3 > d4
+console.log(taqqos3)
+
+let v1 = 3
+let v2 = 2
+let v3 = 12
+let qoshilish = (v1*v2)+v3
+let v4 = 15
+let taqqoslash = qoshilish >= v4
+console.log(taqqoslash)
+
+let n1 = 8
+let n2 = 2
+let kvadrat = (n1**n2)
+let num = 50
+let taqqoslash1 = kvadrat > num
+console.log(taqqoslash1)
+
+let v5 = 6
+let v6 = 2
+let v7 = 5
+let qoshilish1 = (v5 / v6)+v7
+let v8 = 10
+let taqqoslash3 = qoshilish1 <= v8
+console.log(taqqoslash3)
+
+
+
+
+
